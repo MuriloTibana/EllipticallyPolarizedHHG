e0e96a6fe7f02fa61a0a72d2ef71ebf3a928063c
Murilo Tibana <tibanamurilo@hotmail.com>
2024-07-19 12:06:21 -0600

    Fixed phase variable

--- Ellipticity_TimeDelay.m
+++ Ellipticity_TimeDelay.m
@@ -1,6 +1,8 @@
 clc; clear; close all;
 
 path = 'results/';
+pathData = 'data/';
+
 dataFiles = dir(fullfile(path, '*.txt'));
 dataFiles = natsortfiles(dataFiles);
 
@@ -8,9 +10,11 @@ for i = 1:1
     dataName = dataFiles(i).name;
     dash_index = strfind(dataName, '_');
     dot_index = strfind(dataName, '.');
+
     time_delay = dataName(dash_index(2)+1:dot_index - 1);
     
     data = readmatrix(fullfile(path, dataName));
+    disp(['Calculating Stuff for: ' dataName])
     t = data(:,1);
     Sx = data(:,2);
     Sy = data(:,3);
@@ -25,10 +29,10 @@ for i = 1:1
     f = f/0.057;                            % Omega/omega
     
     % Calculate Ellipticity
-    phi_x = 0;
-    phi_y = -pi/4;
-    rho = abs(SY./SX);
-    epsilon = -tan(0.5*asin((2*rho./(1 + rho.^2))*sin(phi_y - phi_x)));
+    phi_x = angle(SX);
+    phi_y = angle(SY);
+    rho = abs(sqrt(SY./SX));
+    epsilon = -tan(0.5*asin((2*rho./(1 + rho.^2)).*sin(phi_y - phi_x)));
     sigma = 0.125;
     N = 1:35;
 
@@ -37,19 +41,11 @@ for i = 1:1
 
     for k = N
         weighting = exp(-0.5 * ((f - k).^2) / sigma^2);
-        
         epsilon_weighted(k) = sum(epsilon .*((abs(S)).^2).* weighting) / sum(((abs(S)).^2) .* weighting);
     end
 
-    % Plot the high harmonics spectrum
-    figure(i);
-    plot(f, abs(SX), f, abs(SY), 'LineWidth', 1);
-    xlabel('Harmonic Order (multiples of $\omega$)', 'Interpreter','latex');
-    ylabel('Componnent of HHG', 'Interpreter','latex');
-    legend('|S_x|^2', '|S_y|^2')
-    title(['High Harmonics Spectrum - Time Delay: ' time_delay]);
-    yscale log
-    xlim([0 36])
+    save(fullfile(pathData, [dataName(1:dot_index - 1), '.mat']), "t", "SX", "SY", "epsilon_weighted", "f", "N");
+    clear t Sx Sy SX SY f rho epsilon epsilon_weighted;
 end
 
 
